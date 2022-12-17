e750e1394282a56bc4c9b8edbbd9434e1a7d2cc0
SJSL01 <120094702+SJSL01@users.noreply.github.com>
2022-12-17 17:41:13 +0530

    fixes

--- server-file/app.js
+++ server-file/app.js
@@ -3,9 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
 const connect = require("./dbConnect")
+const cors = require("cors")
 dotenv.config()
 
-
+app.use(cors())
 app.use(express.json({ limit: "5mb" }));
 app.use("/", require("./routes/posts"));
 
